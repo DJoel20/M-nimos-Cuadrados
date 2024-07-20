e24cf481dc335b38b118ffdbdc52b8266e1a5a8a
Darlin Joel Anacicha <105738470+DJoel20@users.noreply.github.com>
2024-07-19 20:25:43 -0500

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,5 @@
+from .linear_syst_methods import eliminacion_gaussiana, descomposicion_LU, resolver_LU, matriz_aumentada, separar_m_aumentada  # type: ignore
+from .min_cuadrados import ajustar_min_cuadrados  # type: ignore
+
+from .iterative_methods import gauss_jacobi, gauss_seidel  # type: ignore
+from .min_cuadrados import ajustar_min_cuadrados, der_parcial_2, der_parcial_1, der_parcial_0
